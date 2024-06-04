a5c80b6d50a8b72af56dfa1a0e2ac037587faf75
Boris T <boristrakhon@gmail.com>
2024-06-05 01:22:46 +0300

    My Soluton

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 3: 
+// Задайте произвольный массив. 
+// Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+void PrintConvers(int length, int[] array)
+{
+    if (length == 0) return;
+    Console.Write($"{array[length - 1]} ");
+    PrintConvers(length - 1, array);
+}
+
+int[] array = new int[6] { 1, 3, 0, 15, 7, 28 };
+int lengtharray = array.Length;
+
+PrintConvers(lengtharray, array);
